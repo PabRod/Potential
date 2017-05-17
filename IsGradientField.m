d79805025fa273335966c17b9edf412cf660a5bb
Pablo Rodríguez Sánchez <pablo.rodriguez.sanchez@gmail.com>
2017-05-17 11:55:54 +0200

    Improved IsGradientField and tests

--- IsGradientField.m
+++ IsGradientField.m
@@ -8,68 +8,116 @@ function isGradient = IsGradientField(field)
 %
 % Pablo Rodríguez-Sánchez, March 2017
 
-%% Determine the input size
-% Surely this can be done in a cleaner way
-try
-    x = 0;
-    field(x);
-    dims = 1;
-catch
-    try
-        x = [0, 0];
-        field(x);
-        dims = 2;
-    catch
-        try
-            x = [0, 0, 0];
-            field(x);
-            dims = 3;
-        catch
-            error('Only systems up to 3D are supported');
+switch class(field)
+    case {'symfun', 'sym'}
+        % The algorithm is dimension-dependent...
+        names = argnames(field);
+        dimension = numel(names(:));
+        
+        % ... It is based in the fact that the skew part of the jacobian is 
+        % zero everywhere for gradient fields
+        
+        switch dimension
+            case 1
+                % The skew part of a 1-D jacobian is always zero
+                Jskew = 0;
+                
+            case 2
+                J = jacobian(field);
+                testPoint = rand(1, 2);
+                Jskew = SkewSymmDecomposition(eval(J(testPoint(1), testPoint(2))));
+                
+            case 3
+                J = jacobian(field);
+                testPoint = rand(1, 3);
+                Jskew = SkewSymmDecomposition(eval(J(testPoint(1), testPoint(2), testPoint(3))));
+
+            otherwise
+                ThrowWrongDimensionException();
         end
-    end
-    
+        
+        isGradient = (sum(abs(Jskew(:))) == 0); % True iff all elements are zero
+        return;
+                 
+    case 'function_handle'
+        %% Determine the input size
+        % Surely this can be done in a cleaner way
+        try
+            x = 0;
+            field(x);
+            dims = 1;
+        catch
+            try
+                x = [0, 0];
+                field(x);
+                dims = 2;
+            catch
+                try
+                    x = [0, 0, 0];
+                    field(x);
+                    dims = 3;
+                catch
+                    ThrowWrongDimensionException();
+                end
+            end
+            
+        end
+        
+        %% Check using an integration along a random closed ellipse as a test
+        % Any integral of a gradient field along any closed curve should equal 0.
+        % Here, we only test a small subset of all possible curves. That is why the
+        % method can throw false positives
+        absTol = 1e-9;
+        switch dims
+            case 1 % One-dimensional fields are always gradient
+                isGradient = true;
+                return;
+            case 2
+                % Create a parametric ellipse
+                center = 10*rand(1, dims);
+                rads = 5*rand(1, dims);
+                
+                curve = @(t) center + rads.*[cos(t), sin(t)];
+                dcurve = @(t) rads.*[-sin(t), cos(t)];
+                tmin = -pi;
+                tmax = pi;
+                
+                % Integrate along it
+                closedIntegral = PathIntegral(field, curve, dcurve, tmin, tmax);
+                
+                % Check if it adds up to zero
+                isGradient = (abs(closedIntegral) < absTol);
+                return;
+            case 3
+                % Create a parametric ellipse
+                center = 10*rand(1, dims);
+                rads = 5*rand(1, dims);
+                
+                curve = @(t) center + rads.*[cos(t), sin(t), 0];
+                dcurve = @(t) rads.*[-sin(t), cos(t), 0];
+                tmin = -pi;
+                tmax = pi;
+                
+                % Integrate along it
+                closedIntegral = PathIntegral(field, curve, dcurve, tmin, tmax);
+                
+                % Check if it adds up to zero
+                isGradient = (abs(closedIntegral) < absTol);
+                return;
+            otherwise
+                
+        end
+        
 end
 
-%% Check using an integration along a random closed ellipse as a test
-% Any integral of a gradient field along any closed curve should equal 0.
-% Here, we only test a small subset of all possible curves. That is why the
-% method can throw false positives
-absTol = 1e-9;
-switch dims
-    case 1 % One-dimensional fields are always gradient
-        isGradient = true;
-        return;
-    case 2
-        % Create a parametric ellipse
-        center = 10*rand(1, dims);
-        rads = 5*rand(1, dims);
-        
-        curve = @(t) center + rads.*[cos(t), sin(t)];
-        dcurve = @(t) rads.*[-sin(t), cos(t)];
-        tmin = -pi;
-        tmax = pi;
-        
-        % Integrate along it
-        closedIntegral = PathIntegral(field, curve, dcurve, tmin, tmax);
+end
 
-        % Check if it adds up to zero
-        isGradient = (abs(closedIntegral) < absTol);
-        return;
-    case 3
-        % Create a parametric ellipse
-        center = 10*rand(1, dims);
-        rads = 5*rand(1, dims);
-        
-        curve = @(t) center + rads.*[cos(t), sin(t), 0];
-        dcurve = @(t) rads.*[-sin(t), cos(t), 0];
-        tmin = -pi;
-        tmax = pi;
-        
-        % Integrate along it
-        closedIntegral = PathIntegral(field, curve, dcurve, tmin, tmax);
+function ThrowWrongDimensionException()
+%THROWWRONGDIMENSIONEXCEPTION Throws an exception when systems with more
+%than 3 dimensions are introduced
 
-        % Check if it adds up to zero
-        isGradient = (abs(closedIntegral) < absTol);
-        return;
+msgId = 'IsGradientField:WrongDimension';
+errMsg = 'Only systems up to 3D are supported';
+error(msgId, errMsg);
+
 end
--- tests/testIsGradientField.m
+++ tests/testIsGradientField.m
@@ -1,29 +1,90 @@
-%% 1D
+%% 1D function_handle
 field = @(x) cos(x).*sin(x);
 isGradient = IsGradientField(field);
 
 assert(isGradient);
 
-%% 2D gradient
+%% 2D gradient function_handle
 field = @(x) [x(2), x(1)];
 isGradient = IsGradientField(field);
 
 assert(isGradient);
 
-%% 2D non gradient
+%% 2D non gradient function_handle
 field = @(x) [x(2), x(1).*x(2)];
 isGradient = IsGradientField(field);
 
 assert(~isGradient);
 
-%% 3D gradient
+%% 3D gradient function_handle
 field = @(x) [x(2).*x(3), x(1).*x(3), x(2).*x(3)];
 isGradient = IsGradientField(field);
 
 assert(isGradient);
 
-%% 3D non gradient
+%% 3D non gradient function_handle
 field = @(x) [x(2).*x(3), x(3), x(2).*x(3)];
 isGradient = IsGradientField(field);
 
 assert(~isGradient);
+
+%% 1D symfun
+syms x;
+field(x) = cos(x).*sin(x);
+isGradient = IsGradientField(field);
+
+assert(isGradient);
+
+%% 2D gradient symfun
+syms x y;
+field(x,y) = [y, x];
+isGradient = IsGradientField(field);
+
+assert(isGradient);
+
+%% 2D non gradient symfun
+syms x y;
+field(x,y) = [y, x.*y];
+isGradient = IsGradientField(field);
+
+assert(~isGradient);
+
+%% 3D gradient symfun
+
+syms x y z;
+field(x, y, z) = [y.*z, x.*z, x.*y];
+isGradient = IsGradientField(field);
+
+assert(isGradient);
+
+%% 3D non gradient symfun
+syms x y z;
+field(x, y, z) = [y.*z, z, y.*z];
+isGradient = IsGradientField(field);
+
+assert(~isGradient);
+
+% Usability tests
+
+%% Too many dimensions Function_handle
+field = @(x) [x(2).*x(3), x(1).*x(3), x(2).*x(3), x(4)];
+
+try
+    isGradient = IsGradientField(field); % Too many dimensions
+    assert(false, 'Exception failed to be thrown');
+catch me
+    expectedError = 'IsGradientField:WrongDimension';
+    assert(strcmp(me.identifier, expectedError));
+end
+
+%% Too many dimensions SymFun
+syms x y z u;
+field(x, y, z, u) = [y.*z, z, y.*z, u];
+
+try
+    isGradient = IsGradientField(field); % Too many dimensions
+    assert(false, 'Exception failed to be thrown');
+catch me
+    expectedError = 'IsGradientField:WrongDimension';
+    assert(strcmp(me.identifier, expectedError));
+end
